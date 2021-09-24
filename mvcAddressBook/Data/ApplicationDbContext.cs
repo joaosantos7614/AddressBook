d20c8cc63867082e16bdb972594d680bf00df8aa
João Pedro <joaopedrosantos@gmail.com>
2021-09-24 18:19:36 +0100

    Major structure is already done
    Some problem with the image loading is yet to be solved...

---
+++ mvcAddressBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcAddressBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcAddressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        
+        }
+    }
+}
---
+++ mvcAddressBook/Services/Interfaces/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcAddressBook.Services.Interfaces
+{
+    public interface IImageService // had to change to public to solve accessibility issue
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
